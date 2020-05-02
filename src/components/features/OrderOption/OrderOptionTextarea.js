ef378e5794011cfaabed33ef7ecd1f046c0f19a9
Wojtek Nowak <billnewmann@gmail.com>
2020-05-02 21:38:05 +0100

    Finished OrderOptionTextarea

--- src/components/features/OrderOption/OrderOptionTextarea.js
+++ src/components/features/OrderOption/OrderOptionTextarea.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import styles from './OrderOption.scss';
+import PropTypes from 'prop-types';
 
-
-const OrderOptionsTextarea = () => (
-  <div>
-    <h4>CHECKBOXES</h4>
+const OrderOptionTextarea = ({setOptionValue, defaultValue}) => (
+  <div className={styles.component}>
+    <textarea
+      className={styles.input}
+      placeholder={defaultValue}
+      onChange={event => setOptionValue(event.currentTarget.value)}
+      required
+      maxLength='45'
+    />
   </div>
 );
 
+OrderOptionTextarea.propTypes = {
+  defaultValue: PropTypes.any,
+  setOptionValue: PropTypes.any,
+};
 
-export default OrderOptionsTextarea;
+
+export default OrderOptionTextarea;
