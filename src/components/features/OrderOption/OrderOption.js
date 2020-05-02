18a8e33f797fdd20b4b19aa567bd8bb93567baf1
Wojtek Nowak <billnewmann@gmail.com>
2020-05-02 17:48:20 +0100

    Finished OrderOptionDropdown.js; modified OrderOption.js

--- src/components/features/OrderOption/OrderOption.js
+++ src/components/features/OrderOption/OrderOption.js
@@ -13,7 +13,7 @@ const optionTypes = {
   number: OrderOptionNumber,
 };
 
-const OrderOption = ({name, type, ...otherProps}) => {
+const OrderOption = ({name, type, id, setOrderOption, ...otherProps}) => {
   const OptionComponent = optionTypes[type];
   if(!OptionComponent){
     return null;
@@ -23,6 +23,7 @@ const OrderOption = ({name, type, ...otherProps}) => {
         <h3 className={styles.title}>{name}</h3>
         <OptionComponent
           {...otherProps}
+          setOptionValue={value => setOrderOption({ [id]: value })}
         />
       </div>
     );
--- src/components/features/OrderOption/OrderOptionDropdown.js
+++ src/components/features/OrderOption/OrderOptionDropdown.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import styles from './OrderOption.scss';
+import {formatPrice} from '../../../utils/formatPrice.js';
+import PropTypes from 'prop-types';
 
-
-const OrderOptionsDropdown = () => (
-  <div>
-    <h4>DROPDOWN</h4>
-  </div>
+const OrderOptionDropdown = ({values, required, currentValue, setOptionValue}) => (
+  <select
+    className={styles.dropdown}
+    value={currentValue}
+    onChange={event => setOptionValue(event.currentTarget.value)}
+  >
+    {required ? '' : (
+      <option key='null' value=''>---</option>
+    )}
+    {values.map(value => (
+      <option key={value.id} value={value.id}>{value.name} ({formatPrice(value.price)})</option>
+    ))}
+  </select>
 );
 
+OrderOptionDropdown.propTypes = {
+  values: PropTypes.any,
+  required: PropTypes.any,
+  currentValue: PropTypes.any,
+  setOptionValue: PropTypes.any,
+};
 
-export default OrderOptionsDropdown;
+
+export default OrderOptionDropdown;
