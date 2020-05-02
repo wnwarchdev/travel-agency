70d6d8cfe99fbe9d94d13ca8e00cfb3ede212dc7
Wojtek Nowak <billnewmann@gmail.com>
2020-05-02 23:12:46 +0100

    Finished OrderOptionDate.js

--- src/components/features/OrderOption/OrderOptionDate.js
+++ src/components/features/OrderOption/OrderOptionDate.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import 'react-datepicker/dist/react-datepicker-cssmodules.css';
+import DatePicker from 'react-datepicker';
+import styles from './OrderOption.scss';
+import PropTypes from 'prop-types';
+
+class OrderOptionDate extends React.Component {
+
+  static propTypes = {
+    currentValue: PropTypes.any,
+    setOptionValue: PropTypes.any,
+  }
 
 
-const OrderOptionsDate = () => (
-  <div>
-    <h4>CHECKBOXES</h4>
-  </div>
-);
+  render() {
+    const {currentValue, setOptionValue} = this.props;
+    return (
+      <DatePicker
+        selected={currentValue}
+        onChange={setOptionValue}
+        className={styles.inputSmall}
+        placeholderText={'departure'}
+        dateFormat="dd/MM/yyyy"
+        withPortal
+      />
+    );
+  }
+}
 
-
-export default OrderOptionsDate;
+export default OrderOptionDate;
