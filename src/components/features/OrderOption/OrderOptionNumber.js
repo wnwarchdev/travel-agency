24077ec11a7563bf4c7f58337655e9df39a95534
Wojtek Nowak <billnewmann@gmail.com>
2020-05-02 19:36:06 +0100

    Modified OrderOptionNumber.js - limits not working

--- src/components/features/OrderOption/OrderOptionNumber.js
+++ src/components/features/OrderOption/OrderOptionNumber.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import styles from './OrderOption.scss';
+import PropTypes from 'prop-types';
+import {formatPrice} from '../../../utils/formatPrice.js';
 
-
-const OrderOptionsNumber = () => (
-  <div>
-    <h4>NUMBERS</h4>
+const OrderOptionNumber = ({currentValue, price, setOptionValue, limits}) => (
+  <div className={styles.number}>
+    <input
+      type="number"
+      className= {styles.inputSmall}
+      value={currentValue}
+      onChange={event => setOptionValue(event.currentTarget.value)}
+      min={limits.min}
+      max={limits.max}
+    >
+    </input>
+    x{formatPrice(price)}
   </div>
 );
 
+OrderOptionNumber.propTypes = {
+  currentValue: PropTypes.any,
+  setOptionValue: PropTypes.any,
+  limits: PropTypes.any,
+  price: PropTypes.any,
+};
 
-export default OrderOptionsNumber;
+
+
+export default OrderOptionNumber;
