15c6cada3c6abe662a0a7e201b38f9c1f674fb57
Wojtek Nowak <billnewmann@gmail.com>
2020-05-03 13:38:54 +0100

    Start working on tests

---
+++ src/components/layout/Hero/Hero.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import Hero from './Hero';
+
+describe('Component Hero', () => {
+
+  it('should render without crashing', () => {
+    const component = shallow(<Hero titleText='Lorem ipsum' />);
+    expect(component).toBeTruthy();
+  });
+
+  it('should throw error without required props', () => {
+    expect(() => shallow(<Hero />)).toThrow();
+  });
+
+  it('should render correct title', () => {
+    const expectedTitle = 'Lorem ipsum';
+    const component = shallow(<Hero titleText={expectedTitle} />);
+    console.log(component.debug());
+
+    const renderedTitle = component.find('.title').text();
+    expect(renderedTitle).toEqual(expectedTitle);
+  });
+
+
+
+
+
+});
