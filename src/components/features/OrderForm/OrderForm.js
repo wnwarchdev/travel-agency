a6123d04c4edac4ac468bae136e5dc37be1f2385
Wojtek Nowak <billnewmann@gmail.com>
2020-05-06 23:57:15 +0100

    Add settings.js Modified package and OrderForm (button)

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -7,6 +7,39 @@ import PageTitle from '../../common/PageTitle/PageTitle.js';
 import OrderSummary from '../OrderSummary/OrderSummary.js';
 import OrderOption from '../OrderOption/OrderOption.js';
 import pricing from '../../../data/pricing.json';
+import {formatPrice} from '../../../utils/formatPrice.js';
+import {calculateTotal} from '../../../utils/calculateTotal.js';
+import Button from '../../common/Button/Button';
+import settings from '../../../data/settings';
+
+
+const sendOrder = (options, tripCost) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+  const payload = {
+    ...options,
+    totalCost,
+  };
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  fetch(url, fetchOptions)
+    .then(function(response){
+      return response.json();
+    }).then(function(parsedResponse){
+      console.log('parsedResponse', parsedResponse);
+    });
+};
+
 
 class OrderForm extends React.Component {
 
@@ -30,6 +63,7 @@ class OrderForm extends React.Component {
             <PageTitle text='Trip options' />
             <OrderSummary cost={cost} options={options} />
           </Col>
+          <Button onClick={() => sendOrder(options, cost)}>Order now!</Button>
         </Row>
       </Grid>
     );
