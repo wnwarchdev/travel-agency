5da9a280118ff2830b8a7e67e300204f150a79db
Wojtek Nowak <billnewmann@gmail.com>
2020-05-31 14:56:14 +0100

    Add HappyHourAd components and start working on tests

---
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -0,0 +1,17 @@
+import React from 'react';
+//import ProtoTypes from 'prop-types';
+//import styles from './HappyHourAd.scss';
+
+
+class HappyHourAd extends React.Component {
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+
+export default HappyHourAd;
